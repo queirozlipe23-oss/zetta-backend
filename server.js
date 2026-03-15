b22ebc7e1fa7e07bb4ba9f8c1d10582120ae7f52
queirozlipe23-oss <queirozlipe23@gmail.com>
2026-03-15 01:40:32 -0300

    server.js

--- server.js
+++ server.js
@@ -10,16 +10,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/stock/:symbol", async (req, res) => {
-  const symbol = req.params.symbol;
-
   try {
-    const response = await axios.get(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`
-    );
+    const symbol = req.params.symbol;
+
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
+
+    const response = await axios.get(url);
 
     res.json(response.data);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar dados da ação" });
+    res.status(500).json({
+      error: "Erro ao buscar dados",
+      details: error.message
+    });
   }
 });
 
@@ -27,8 +30,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
-});
-"dependencies": {
-  "express": "^4.18.2",
-  "axios": "^1.6.0"
-}
+}); 
