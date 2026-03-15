0afb46e7230abcd0650474ddb00280a1e70f863f
queirozlipe23-oss <queirozlipe23@gmail.com>
2026-03-15 01:10:16 -0300

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
+const API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
+
 app.get("/", (req, res) => {
   res.send("Zetta API funcionando 🚀");
+});
+
+app.get("/stock/:symbol", async (req, res) => {
+  const symbol = req.params.symbol;
+
+  try {
+    const response = await axios.get(
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar dados da ação" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
